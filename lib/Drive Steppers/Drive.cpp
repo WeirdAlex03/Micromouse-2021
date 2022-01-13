a3705d348558f9660fce68fdeb7a994e9a0504a7
Marcos_ <76269829+delaosacruzmarcos@users.noreply.github.com>
2022-01-13 13:46:43 -0500

    Update Drive.cpp

--- lib/Drive Steppers/Drive.cpp
+++ lib/Drive Steppers/Drive.cpp
@@ -15,6 +15,25 @@ Drive::Drive(uint16_t steps, int diameter, uint8_t Rport, uint8_t Lport){
 }
 
 int Drive::forward(int dis){
+    int turns = dis/(_radius * _theta);
+    for (int idx = 0; idx < turns; idx++){
+        _right.step(1, FORWARD, SINGLE);
+        _left.step(1, FORWARD, SINGLE);
+    }
+    switch ( _ori){
+        case 0:
+        _y = _y + dis;
+        break;
+        case 1:
+        _x = _x + dis;
+        break;
+        case 2:
+        _y = _y - dis;
+        break;
+        case 3:
+        _x = _x - dis;
+        break;
+    }
     return 0;
 }
 
@@ -27,10 +46,50 @@ int Drive::turnLeft(){
         delayMicroseconds(200);
     }
     if(_ori = 0){_ori = 3;}
-
+    else{_ori--;}
     return 0;
 }
 
 int Drive::turnRight(){
+    int turns = floor(_theta * 90);
+    for (int idx = 0; idx < turns; idx++){
+        _left.step(1, FORWARD, SINGLE);
+        delayMicroseconds(200);
+        _right.step(1, BACKWARD, SINGLE);
+        delayMicroseconds(200);
+    }
+    if(_ori = 3){_ori = 0;}
+    else{_ori++;}
+    return 0;
+}
+
+int Drive::turnAround(){
+    int turns = floor(_theta * 180);
+    for (int idx = 0; idx < turns; idx++){
+        _left.step(1, FORWARD, SINGLE);
+        delayMicroseconds(200);
+        _right.step(1, BACKWARD, SINGLE);
+        delayMicroseconds(200);
+    }
+    switch(_ori){
+        case(0):
+            _ori = 2;
+            break;
+        case(1):
+            _ori = 3;
+            break;
+        case(2):
+            _ori = 0;
+            break;
+        case(3):
+            _ori = 1;
+            break;
+    }
+    return 0;
+}
+
+int Drive::setSpeed(int speed){
+    _left.setSpeed(speed);
+    _right.setSpeed(speed);
     return 0;
 }
